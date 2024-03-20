cd44718efa064165deef8155ecf460046b1ea894
ccmiguel <105742725+ccmiguel@users.noreply.github.com>
2024-03-20 17:59:31 +0000

    cambiops

--- semana5/10_monolith/client.py
+++ semana5/10_monolith/client.py
@@ -11,26 +11,22 @@ print(response.text)
 
 #POST
 json = {
-    1: {
+    
         "title": "Mi experiencia como dev",
         "content": "¡Hola mundo! Esta es mi primera experiecia como dev.",
-    },
-    2: {
-        "title": "Otra publicación",
-        "content": "¡Bienvenidos a mi blog! Aquí hay otra publicación.",
-    },
+    
 }
-response = requests.get(f"{url}/post", data=json)
+response = requests.get(f"{url}/posts", data=json)
 print(response.text)
 
 #put
 put_data = {
-    1: {
+    
         "title": "Mi experiencia como estudiante de la UMSA",
         "content": "¡Hola mundo! Esta es mi primera experiecia como estudiante.",
-    }
+    
 }
-response = requests.put(f"{url}/post", data=put_data)
+response = requests.put(f"{url}/post/1", data=put_data)
 print(response.text)
 
 #delete
